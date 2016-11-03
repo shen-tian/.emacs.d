0d196d3a30717d8886f6b1136394a500a7362f19
Shen Tian <biozinc@gmail.com>
2016-11-03 09:46:37 +0200

    Removing unused theme. Slight cleanup of .gitignore

--- init.el
+++ init.el
@@ -143,14 +143,10 @@
  '(global-linum-mode t)
  '(package-selected-packages
    (quote
-    (magit-annex
-     tagedit
-     smex
-     rainbow-delimiters
-     projectile
-     paredit
-     magit
-     ido-ubiquitous
-     exec-path-from-shell
-     clojure-mode-extra-font-locking
-     cider))))
+    (markdown-mode magit-annex tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
