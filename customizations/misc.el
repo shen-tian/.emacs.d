cccc0843bd33ecc877f72c13aaf75a7b8d669248
Shen Tian <biozinc@gmail.com>
2020-05-19 09:27:48 +0200

    Allow find-file to create dirs too

--- customizations/misc.el
+++ customizations/misc.el
@@ -10,3 +10,11 @@
 
 ;; Go straight to scratch buffer on startup
 (setq inhibit-startup-message t)
+
+;; Make find-file more useful
+(defadvice find-file (before make-directory-maybe (filename &optional wildcards) activate)
+  "Create parent directory if not exists while visiting file."
+  (unless (file-exists-p filename)
+    (let ((dir (file-name-directory filename)))
+      (unless (file-exists-p dir)
+        (make-directory dir t)))))
